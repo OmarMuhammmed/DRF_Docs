28a7bc0ec62df89a40f208f32c9c19069f4dd78e
OmarMuhammmed <amorey2006@gmail.com>
2024-12-01 08:20:25 +0200

    feat: custom serilizer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -7,12 +7,29 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta :
         model = Movie
         fields = ['pk','hall','movie']
+
+    def to_representation(self, instance):
+
+        """Convert `movie` to lowercase."""
+
+        ret = super().to_representation(instance)
+        ret['movie'] = ret['movie'].lower()
+        return ret
         
 class ReservationSerializer(serializers.ModelSerializer):
 
     class Meta :
         model = Reservation
         fields = ['pk','client','movie']
+
+    # Works When Write in views.py serilzer.save() 
+    def create(self, validated_data):
+        return Reservation.objects.create(validated_data)  
+
+     # Works When Write in views.py serilzer.save()
+    def update(self, instance, validated_data):
+        instance.client = validated_data.get('client', instance.client)
+        return instance     
 
     
 
